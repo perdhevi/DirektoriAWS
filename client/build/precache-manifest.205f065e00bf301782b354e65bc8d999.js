16fc2e2e62c2556cc9717d2248c45234d2c86a42
Raditya Perdhevi <raditya@perdhevi.com>
2020-07-04 01:56:04 +0700

    chores:initial build

---
+++ client/build/precache-manifest.205f065e00bf301782b354e65bc8d999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edd1a4deee36880e4d9960c3b25b8df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa9c9b145d59c998d47",
+    "url": "/static/js/2.d3807240.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.d3807240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c6721724fb1cd236e2",
+    "url": "/static/js/main.1bbf94cd.chunk.js"
+  },
+  {
+    "revision": "53cb1501e01a309b998f",
+    "url": "/static/js/runtime-main.0bfbe608.js"
+  }
+]);
